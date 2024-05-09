3e30715ca687abc75dcf64e7457b66cc3cdd078b
白子敬 <zijjing@baizijingdeMacBook-Air.local>
2024-05-09 23:32:39 +0800

    final_test

---
+++ common/common.py
@@ -0,0 +1,23 @@
+# 引入模塊
+
+# 基本測試模塊
+import requests
+import pytest
+
+# http標頭模塊
+from wsgiref import headers
+
+# 解析ini檔的模塊
+from configparser import ConfigParser
+
+# 模組允許我們與操作系統溝通，例如處理文件路徑、環境變數、檔案和目錄等
+import os
+
+# yaml_name模塊
+import yaml
+
+# 讀取,寫入配置文件
+import configparser
+
+
+
